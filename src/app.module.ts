2aecf09b1a9548e942ea92cf0d91cfca3f14bd21
Adwaith Raj <adwaithraj45654@gmail.com>
2024-05-31 19:54:21 +0530

    prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true,
       envFilePath: '.env',
     }),
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
