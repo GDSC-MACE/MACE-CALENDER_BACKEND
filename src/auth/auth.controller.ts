ae549500cef03bd171de52a9bba6f5ec05647564
Jithin-caz <117753804+Jithin-caz@users.noreply.github.com>
2024-06-02 18:09:19 +0530

    logout done

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, UseGuards, Post, Request } from '@nestjs/common';
+import { Controller, Get, UseGuards, Post, Request,Session,Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthenticatedGuard } from './Guards/authenticated.guard';
 import { LocalAuthGuard } from './Guards/local-auth.guard';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -11,5 +12,22 @@ export class AuthController {
   @Post('login')
   login(@Request() req): any {
     return { message: 'login success!!!!',user:req.user };
+  }
+
+
+  @UseGuards(AuthenticatedGuard) 
+  @Get('logout') 
+  logout(
+    @Session() session: Record<string, any>,
+    @Res() res: Response,
+  ) {
+    console.log(session.id);
+    session.destroy((err) => {
+      if (err) {
+        return res.status(500).json({ message: 'Error' });
+      }
+      res.clearCookie('connect.sid');
+      return res.status(200).json({ message: 'User logged out' });
+    });
   }
 }
