1b2e2dedda0a4198d9dac352977255bbdcdddc26
ADWAITH RAJ <117984453+adw-ith@users.noreply.github.com>
2024-06-06 23:50:51 +0530

    cors

--- src/main.ts
+++ src/main.ts
@@ -38,6 +38,10 @@ async function createSuperUser(prisma: PrismaService) {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ["*"],
+    credentials: true,
+  });
   const prismaService = app.get(PrismaService);
   await prismaService.$connect();
   await createSuperUser(prismaService);
